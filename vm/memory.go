9e66cb3ea19e664b9d7796c7c168dd28f5d6a916
Lennart Espe <lennart@mooxmirror.io>
2016-09-19 20:30:29 +0200

    fix: missing address in errors

--- vm/memory.go
+++ vm/memory.go
@@ -2,43 +2,56 @@ package vm
 
 import (
 	"encoding/binary"
-	"errors"
+	"fmt"
 )
 
 type Memory interface {
 	Load(addr uint16) (uint16, error)
 	Store(addr, value uint16) error
 	StoreByte(addr uint16, value byte) error
+	Segment(from, to uint16) []byte
 	Convert(value uint16) []byte
-	Size() uint16
+	InRange(addr uint16) bool
+}
+
+type OutOfRangeError struct {
+	Address uint16
+}
+
+func (err OutOfRangeError) Error() string {
+	return fmt.Sprintf("0x%4.4X is out of memory range", err.Address)
 }
 
 type randomAccessMemory []byte
 
-func (memory randomAccessMemory) Size() uint16 {
-	return uint16(len(memory))
+func (memory randomAccessMemory) InRange(addr uint16) bool {
+	return int(addr) < len(memory)
 }
 func (memory randomAccessMemory) Load(addr uint16) (uint16, error) {
-	if addr >= memory.Size() {
-		return 0, errors.New("memory address out of range")
+	if !memory.InRange(addr) {
+		return 0, &OutOfRangeError{addr}
 	}
 	return binary.BigEndian.Uint16(memory[addr : addr+2]), nil
 }
 
 func (memory randomAccessMemory) Store(addr, value uint16) error {
-	if addr >= memory.Size() {
-		return errors.New("memory address out of range")
+	if !memory.InRange(addr) {
+		return &OutOfRangeError{addr}
 	}
 	binary.BigEndian.PutUint16(memory[addr:addr+2], value)
 	return nil
 }
 
 func (memory randomAccessMemory) StoreByte(addr uint16, value byte) error {
-	if addr >= memory.Size() {
-		return errors.New("memory address out of range")
+	if !memory.InRange(addr) {
+		return &OutOfRangeError{addr}
 	}
 	memory[addr] = value
 	return nil
+}
+
+func (memory randomAccessMemory) Segment(from, to uint16) []byte {
+	return memory[from:to]
 }
 
 func (randomAccessMemory) Convert(value uint16) []byte {
